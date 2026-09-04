76aaa838f4872c673534669e8b18e2cf6aedf774
kummariBharath <bharathkummari2611@gmail.com>
2026-09-04 16:55:02 +0530

    add example for ndmin argument in array creation

--- creating array.py
+++ creating array.py
@@ -47,3 +47,8 @@ arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]]
 print(arr.ndim)
 print(arr1.ndim)
 print(arr2.ndim)
+
+#ndmin argument is used to specify the minimum number of dimensions that the resulting array should have.
+import numpy as np
+arr=np.array([1,2,3,4],ndmin=5)
+print(arr)
