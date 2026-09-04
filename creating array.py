8ab418e02cdfe9e8f827e8cfd6da8f9c6dbf32d9
kummariBharath <bharathkummari2611@gmail.com>
2026-09-04 17:00:31 +0530

    refactor: enhance print statements for array examples and dimensions

--- creating array.py
+++ creating array.py
@@ -1,4 +1,4 @@
-
+"""
 #ndarray is array object in numpy
 #when a list,tuple or any sequence is passed to the array() function, it is converted into an ndarray object.
 import numpy as np
@@ -18,37 +18,37 @@ import numpy as np
 arr=np.array(44)
 print(arr)
 
-
+"""
 #1-D array is an array that has 0-D arrays as its elements
 import numpy as np
 arr=np.array([1,2,3,4,5])
-print(arr)
+print("1-D array:",arr)
 
 #2-D array is an array that has 1-D arrays as its elements
 import numpy as np
 arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
-print(arr)
+print("2-D array:",arr)
 
 #3-D array is an array that has 2-D arrays as its elements
 import numpy as np
 arr1=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
-print(arr1)
+print("3-D array:",arr1)
 
 #4-D array is an array that has 3-D arrays as its elements
 import numpy as np
 arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]],[[19,20,21],[22,23,24]]]])
-print(arr2)
+print("4-D array:",arr2)
 
 #ndim is an attribute that returns an integer that tells us how many dimensions the array have.
 import numpy as np
 arr=np.array([1,2,3,4,5])
 arr1=np.array([[1,2,3,4,5],[6,7,8,9,10]])
 arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]],[[19,20,21],[22,23,24]]]])
-print(arr.ndim)
-print(arr1.ndim)
-print(arr2.ndim)
+print("1-D array dimensions:",arr.ndim)
+print("2-D array dimensions:",arr1.ndim)
+print("3-D array dimensions:",arr2.ndim)
 
 #ndmin argument is used to specify the minimum number of dimensions that the resulting array should have.
 import numpy as np
 arr=np.array([1,2,3,4],ndmin=5)
-print(arr)
+print("Array with minimum dimensions:",arr)
