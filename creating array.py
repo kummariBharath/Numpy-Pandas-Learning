a5f22d69639c49b9841be002701a9ef5683a6d8e
kummariBharath <bharathkummari2611@gmail.com>
2026-09-04 16:38:02 +0530

    add examples for 3-D and 4-D arrays in numpy

--- creating array.py
+++ creating array.py
@@ -28,3 +28,12 @@ print(arr)
 import numpy as np
 arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
 print(arr)
+
+#3-D array is an array that has 2-D arrays as its elements
+import numpy as np
+arr1=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr1)
+
+#4-D array is an array that has 3-D arrays as its elements
+import numpy as np
+arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]],[[19,20,21],[22,23,24]]]])
