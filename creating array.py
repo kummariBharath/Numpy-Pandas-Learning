3e217905137530cf162c260b894cd5e02602ac4f
kummariBharath <bharathkummari2611@gmail.com>
2026-09-04 16:50:44 +0530

    .

--- creating array.py
+++ creating array.py
@@ -37,3 +37,13 @@ print(arr1)
 #4-D array is an array that has 3-D arrays as its elements
 import numpy as np
 arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]],[[19,20,21],[22,23,24]]]])
+print(arr2)
+
+#ndim is an attribute that returns an integer that tells us how many dimensions the array have.
+import numpy as np
+arr=np.array([1,2,3,4,5])
+arr1=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+arr2=np.array([[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]],[[[13,14,15],[16,17,18]],[[19,20,21],[22,23,24]]]])
+print(arr.ndim)
+print(arr1.ndim)
+print(arr2.ndim)
