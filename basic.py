d2b73ef7bc11139de83ba83e19c4cd463a252c8d
kummariBharath <bharathkummari2611@gmail.com>
2026-09-04 16:11:13 +0530

    fix: correct ndarray initialization to use array function

--- basic.py
+++ basic.py
@@ -3,8 +3,8 @@
 #ndarray is array object in numpy
 #when a list,tuple or any sequence is passed to the array() function, it is converted into an ndarray object.
 import numpy as np
-arr=np.ndarray([1,2,3,4,5])
-arr1=np.ndarray((2,3,4,4,4,4))
+arr=np.array([1,2,3,4,5])
+arr1=np.array((2,3,4,4,4,4))
 print(arr)
 print(arr1)
 print(type(arr))
