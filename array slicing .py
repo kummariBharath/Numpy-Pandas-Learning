361d7e7223a1efa902ed3db5b94d6ed19ab520b6
kummariBharath <bharathkummari2611@gmail.com>
2026-09-05 11:59:03 +0530

    add: enhance examples for negative slicing and step arguments in 1-d arrays

--- array slicing .py
+++ array slicing .py
@@ -2,12 +2,23 @@ import numpy as np
 arr=np.array([1,2,3,4,5,6])
 print(arr[0:6]) #start:stop:step
 
-#negative slice 
+#negative slice on 1-d array 
 import numpy as np
 arr=np.array([23,43,44,22,45])
 print(arr[-5:-1]) #slice same array using the negative slice
 print(arr[-5:-1:2]) #slice  the same array using the negative slice with step
 print(arr[-3:-1])#Slice from the index 3 from the end to index 1 from the end 
+
+#using the step argument on 1-d array
+import numpy as np
+arr1=np.array([1,2,3,4,5,6,78,9,20])
+print(arr1[1:7:2])
+print(arr1[::2])# Slice the array from the start to the end with step 2
+print(arr1[::3])# Slice the array from the start to the end with step 3
+print(arr1[-1:-10:-1])# Slice the array from the end to the start with step -1
+print(arr1[-1:-10:-2])# Slice the array from the end to the start with step -2
+print(arr1[::-1])#reverse the array using slicing
+print(arr1[::-2])#reverse the array using slicing with step -2
 
 
 
